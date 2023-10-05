a8db5f7fb37d3fdbdbcc64ea76c0378d79c606f8
Replit user <>
2023-10-05 16:23:48 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -139,7 +139,11 @@ int is_final(Node *n){
   return 1;
 }
 
-Node *DFS(Node *initial, int *cont) { return NULL; }
+Node *DFS(Node *initial, int *cont){
+
+  return NULL;
+}
+
 
 /*
 int main( int argc, char *argv[] ){
